1a5db11f064ff37e11ec8c42324d34626717b85e
Collin Smith <collinsmith70@gmail.com>
2016-11-13 15:28:34 -0800

    Documented Builder

--- src/com/gmail/collinsmith70/builder/Builder.java
+++ src/com/gmail/collinsmith70/builder/Builder.java
@@ -2,8 +2,21 @@ package com.gmail.collinsmith70.builder;
 
 import java.util.function.Supplier;
 
+/**
+ * Interface used to represent a {@code Builder} class for constructing object instances.
+ * {@code Builder} implementations should provide methods to set state, and every method should
+ * return the {@code Builder} instance to allow method chaining which terminates with the terminal
+ * {@link #build()} method.
+ *
+ * @param <T> Type of object which the {@code Builder} creates
+ */
 public interface Builder<T> extends Supplier<T> {
 
+  /**
+   * Builds an instance of {@link T} using the state set by this {@link Builder}.
+   *
+   * @return Instance of type {@link T} created using the values set by this {@link Builder}
+   */
   T build();
 
 }
